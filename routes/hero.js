5251eb113051042a0f7b8035a1241650599b0002
Jinah Lee <44072645+brandnewjinah@users.noreply.github.com>
2024-11-15 04:16:10 -0800

    feat: add api to remove and update hero item (#16)

--- routes/hero.js
+++ routes/hero.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addHeroItem, getAllHero } from "../controllers/hero.js";
+import { addHeroItem, getAllHero, removeHeroItem, updateHeroItem } from "../controllers/hero.js";
 import { checkAdmin } from "../middleware/checkAuth.js";
 
 const router = express.Router();
@@ -9,8 +9,18 @@ const router = express.Router();
 // @access Private
 router.post("/", checkAdmin, addHeroItem);
 
+// @route DETELE /hero
+// @desc Remove a hero item
+// @access Private
+router.delete("/:id", checkAdmin, removeHeroItem);
+
+// @route PATCH /hero
+// @desc Make changes to a hero item
+// @access Private
+router.patch("/:id", checkAdmin, updateHeroItem);
+
 // @route GET /hero
-// @desc Get all hero
+// @desc Get all hero items
 // @access Public
 router.get("/", getAllHero);
 
